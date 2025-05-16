1bd69a31a3f4b2c524129c3e0c1df779889ad4c2
charlotteroymans <charlotte.roymans@student.kuleuven.be>
2025-05-16 16:08:51 +0200

    Update decaan_verkiezing.py

--- decaan_verkiezing.py
+++ decaan_verkiezing.py
@@ -28,3 +28,12 @@ class DecaanKiezer(Kiezer):
             print(f"{self.naam} heeft gestemd op {kandidaat} ({self.opleiding})")
         else:
             print(f"{self.naam} kan niet stemmen op {kandidaat} ({kandidaat.opleiding})")
+lijst_DecaanKandidaten = [DecaanKandidaat('Jeffrey','Fysica'),DecaanKandidaat('Stefaan','Fysica'),DecaanKandidaat('Willem','Wiskunde')]
+lijst_DecaanKiezers = [DecaanKiezer('Jessica','Wiskunde'),DecaanKiezer('Pablo','Fysica'),DecaanKiezer('Sophie','Fysica')]
+
+for kiezer in lijst_DecaanKiezers:
+    for kandidaat in lijst_DecaanKandidaten:
+        kiezer.stem(kandidaat)
+print('Stemmen per kandidaat:')
+for kandidaat in lijst_DecaanKandidaten:
+    print(f'{kandidaat}:{len(kandidaat.stemmen)} stemmen')
